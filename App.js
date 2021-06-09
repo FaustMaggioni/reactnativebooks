91feb0475cb4cf25d1d85a8e5bd1b7de2e22753b
Faustino <Faustino@LAPTOP-VI7AL9FL>
2021-06-08 21:16:56 -0300

    fir

--- App.js
+++ App.js
@@ -4,18 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <Text style={styles.sofi}>Sofia </Text>  
       <StatusBar style="auto" />
-    </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 2,
+    backgroundColor: 'lightgray',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  sofi:{
+    color: 'blue'
+  }
 });
